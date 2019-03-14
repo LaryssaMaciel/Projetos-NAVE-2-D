7122dae6eca155e30add18bb46ed8ae223768953
LaryssaMaciel <maciel.f.laryssa@gmail.com>
2019-03-14 14:52:15 -0300

    Programa que encontra o fibonacci

--- PAD12D19007/Program.cs
+++ PAD12D19007/Program.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PAD12D190307
+namespace PAD12D19007
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            //Programa para descobrir o fibonacci
+
+            Console.WriteLine("Diga um número pra eu descobrir seu fibonacci");
+
+            int valor = int.Parse(Console.ReadLine());
+            int numero1 = 0;
+            int numero2 = 1;
+            int soma = 0;
+
+            for (int i = 0; i < valor; i++)
+            {
+                Console.WriteLine(numero1);
+                soma = numero1 + numero2;
+                numero1 = numero2;
+                numero2 = soma;
+            }
+            Console.ReadLine();
         }
     }
 }
